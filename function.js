0ff336594796a280c6c5d36a33ed163bc955679d
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-04-15 11:37:15 +0700

    Update function.js

--- function.js
+++ function.js
@@ -17,8 +17,8 @@ window.function = function (data, labels, width, height, title, tooltip, unit) {
   <head>
     <meta charset="utf-8">
     <title>Glide Yes-Code</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.7.0/dist/chartjs-plugin-datalabels.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-trendline"></script>
   </head>
   <body>
   <div class="chart-container" style="position: relative; height:${height}vh; width:${width}vw">
@@ -34,15 +34,10 @@ image.src = "https://i.imgur.com/Bo7rl1y.png";
   "data": {
     "datasets": [
       {
-        "label": "TPO",
+        "label": "${tooltip}",
         "backgroundColor": "#55B6C3",
         "borderColor": "#55B6C3",
         "data": [${data}],
-        trendlineLinear: {
-        style: "rgba(255,105,180, .8)",
-        lineStyle: "dotted|solid",
-        width: 2
-        },
         "fill": false,
         "spanGaps": false,
         "lineTension": 0,
@@ -58,7 +53,11 @@ image.src = "https://i.imgur.com/Bo7rl1y.png";
         "type": "line",
         "borderWidth": 3,
         "hidden": false,
-        "yAxisID": "Y1"
+        trendlineLinear: {
+        style: "red",
+        lineStyle: "solid",
+        width: 2
+        }
       }
     ],
     "labels": [${labels}]
@@ -67,35 +66,46 @@ image.src = "https://i.imgur.com/Bo7rl1y.png";
     responsive: true,
     maintainAspectRatio: false,
     layout: {
-        padding: {
-            bottom: 20
+     padding: {
+           left: 5,
+           right: 5,
+           top: 15,
+           bottom: 15
+     }
+    },
+    "scales": {
+        yAxis: {
+          beginAtZero: true,
+          type: 'linear',
+          grace: '10%'
+        },
+        xAxis: {
+          beginAtZero: true,
+          offset: true,
+          ticks: {
+            maxTicksLimit: 7
+          }
         }
     },
-    tooltips: {
+    plugins: {
+    tooltip: {
+        displayColors: false,
         callbacks: {
-            label: function(tooltipItem, data) {
-                return '${tooltip}: ' + data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index] + '${unit}';
+            label: function(context) {
+                    return context.dataset.label + ": " + context.parsed.y + '%';
             }
         }
     },
-    "title": {
+    title: {
       "display": true,
       "text": "${title}",
-      "position": "top",
-      "fontSize": 20,
-      "fontFamily": "BlinkMacSystemFont",
-      "fontColor": "#111111",
-      "fontStyle": "700",
-      "padding": 20,
+      font: {
+      "size": 20,
+      "family": "BlinkMacSystemFont",
+      "color": "#111111",
+      "weight": "700",
       "lineHeight": 1.2
-    },
-    "layout": {
-      "padding": {
-        "left": 0,
-        "right": 0,
-        "top": 0,
-        "bottom": 0
-      }
+    }
     },
     "legend": {
       "display": false,
@@ -110,193 +120,7 @@ image.src = "https://i.imgur.com/Bo7rl1y.png";
         "fontStyle": "normal",
         "padding": 10
       }
-    },
-    "scales": {
-      "xAxes": [
-        {
-          "id": "X1",
-          "display": true,
-          "position": "bottom",
-          "type": "category",
-          "stacked": false,
-          "time": {
-            "unit": false,
-            "stepSize": 1,
-            "displayFormats": {
-              "millisecond": "h:mm:ss.SSS a",
-              "second": "h:mm:ss a",
-              "minute": "h:mm a",
-              "hour": "hA",
-              "day": "MMM D",
-              "week": "ll",
-              "month": "MMM YYYY",
-              "quarter": "[Q]Q - YYYY",
-              "year": "YYYY"
-            }
-          },
-          "distribution": "linear",
-          "gridLines": {
-            "display": true,
-            "color": "rgba(0, 0, 0, 0.1)",
-            "borderDash": [
-              0,
-              0
-            ],
-            "lineWidth": 1,
-            "drawBorder": true,
-            "drawOnChartArea": true,
-            "drawTicks": true,
-            "tickMarkLength": 10,
-            "zeroLineWidth": 1,
-            "zeroLineColor": "rgba(0, 0, 0, 0.25)",
-            "zeroLineBorderDash": [
-              0,
-              0
-            ]
-          },
-          "angleLines": {
-            "display": true,
-            "color": "rgba(0, 0, 0, 0.1)",
-            "borderDash": [
-              0,
-              0
-            ],
-            "lineWidth": 1
-          },
-          "pointLabels": {
-            "display": true,
-            "fontColor": "#666",
-            "fontSize": 10,
-            "fontStyle": "normal"
-          },
-          "ticks": {
-            beginAtZero: true,
-            "display": true,
-            "fontSize": 10,
-            "fontFamily": "sans-serif",
-            "fontColor": "#666666",
-            "fontStyle": "normal",
-            "padding": 0,
-            "stepSize": null,
-            "minRotation": 0,
-            "maxRotation": 50,
-            "mirror": false,
-            "reverse": false
-          },
-          "scaleLabel": {
-            "display": false,
-            "labelString": "Axis label",
-            "lineHeight": 1.2,
-            "fontColor": "#666666",
-            "fontFamily": "sans-serif",
-            "fontSize": 12,
-            "fontStyle": "normal",
-            "padding": 4
-          }
-        }
-      ],
-      "yAxes": [
-        {
-          "id": "Y1",
-          "display": true,
-          "position": "left",
-          "type": "linear",
-          "stacked": false,
-          "time": {
-            "unit": false,
-            "stepSize": 1,
-            "displayFormats": {
-              "millisecond": "h:mm:ss.SSS a",
-              "second": "h:mm:ss a",
-              "minute": "h:mm a",
-              "hour": "hA",
-              "day": "MMM D",
-              "week": "ll",
-              "month": "MMM YYYY",
-              "quarter": "[Q]Q - YYYY",
-              "year": "YYYY"
-            }
-          },
-          "distribution": "linear",
-          "gridLines": {
-            "display": true,
-            "color": "rgba(0, 0, 0, 0.1)",
-            "borderDash": [
-              0,
-              0
-            ],
-            "lineWidth": 1,
-            "drawBorder": true,
-            "drawOnChartArea": true,
-            "drawTicks": true,
-            "tickMarkLength": 10,
-            "zeroLineWidth": 1,
-            "zeroLineColor": "rgba(0, 0, 0, 0.25)",
-            "zeroLineBorderDash": [
-              0,
-              0
-            ]
-          },
-          "angleLines": {
-            "display": true,
-            "color": "rgba(0, 0, 0, 0.1)",
-            "borderDash": [
-              0,
-              0
-            ],
-            "lineWidth": 1
-          },
-          "pointLabels": {
-            "display": true,
-            "fontColor": "#666",
-            "fontSize": 10,
-            "fontStyle": "normal"
-          },
-          "ticks": {
-            beginAtZero: true,
-            "display": true,
-            "fontSize": 12,
-            "fontFamily": "sans-serif",
-            "fontColor": "#666666",
-            "fontStyle": "normal",
-            "padding": 0,
-            "stepSize": null,
-            "minRotation": 0,
-            "maxRotation": 50,
-            "mirror": false,
-            "reverse": false
-          },
-          "scaleLabel": {
-            "display": false,
-            "labelString": "Axis label",
-            "lineHeight": 1.2,
-            "fontColor": "#666666",
-            "fontFamily": "sans-serif",
-            "fontSize": 12,
-            "fontStyle": "normal",
-            "padding": 4
-          }
-        }
-      ]
-    },
-    "plugins": {
-      "datalabels": {
-        "display": false,
-        "align": "center",
-        "anchor": "center",
-        "backgroundColor": "#eee",
-        "borderColor": "#ddd",
-        "borderRadius": 6,
-        "borderWidth": 1,
-        "padding": 4,
-        "color": "#666666",
-        "font": {
-          "family": "sans-serif",
-          "size": 10,
-          "style": "normal"
-        }
-      },
-      "tickFormat": ""
+    }
     },
   }
 }
