9f4208a3ec8d0af30bbac645446269926587b52b
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-06-20 16:30:26 +0700

    Update function.js

--- function.js
+++ function.js
@@ -87,7 +87,7 @@ image.src = "https://i.imgur.com/SkTd1Yc.png";
         displayColors: false,
         callbacks: {
             label: function(context) {
-                return "Total fund: " + context.parsed.y + " USD";
+                return context.dataset.label +": $" + context.parsed.y;
             }
         }
     },
